5be378b2d84b7909f2811ff8622593c962361a71
Thomas <thomasko@gmx.net>
2023-02-23 02:57:44 +0100

    - Organized the rocket into it's own class - Added thrust animation & sound with a new skin

--- core/src/com/mygdx/game/GravityForce.java
+++ core/src/com/mygdx/game/GravityForce.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,41 +14,57 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GravityForce implements Screen {
 
+    Rocket rock;
+
+    static Sprite rocket;
+    Sprite rocketEngineSprite;
     SpriteBatch batch;
-    SpriteBatch UiBatch;
+    SpriteBatch uiBatch;
     OrthographicCamera camera;
     OrthographicCamera uicamera;
 
     Texture rocketImage;
+    Texture rocketEngine;
     Texture leftArrow;
     Texture rightArrow;
     Texture boostTexture;
     Texture background;
+
 
     //Die Rectangles für die Control Buttons
     Rectangle leftButton;
     Rectangle rightButton;
     Rectangle boostButton;
 
-    static Sprite rocket;
-    Vector3 touchPos;
-
+    //Variablen für die Bewegung der Rakete
+    Integer CURRENT_VELOCITY = 0;
     Integer MAX_VELOCITY = 200;
-    Integer gravity = -50;
-    Integer currentVelocity = 0;
-    static boolean moving = false;
+    Integer GRAVITY = -50;
+    Integer THRUST = 125;
 
+    static boolean moving = false;
 
     //Kamera Variablen fürs Folgen der Rakete
     float lerp = 0.05f;
     Vector3 position;
 
+    //Touchposition
+    Vector3 touchPos;
+
+    //Soundvariablen
+    static Sound thrust_sound;
+    long sound_id;
+    float volume;
+    boolean wasPlayed;
+    float soundbuffer;
+
     public GravityForce(final Game game) {
 
-        batch = new SpriteBatch();
-        UiBatch = new SpriteBatch();
+        rock = new Rocket();
 
-        rocketImage = new Texture("rocket.png");
+        batch = new SpriteBatch();
+        uiBatch = new SpriteBatch();
+
         leftArrow = new Texture("leftArrow.png");
         rightArrow = new Texture("rightArrow.png");
         boostTexture = new Texture("boostTexture.png");
@@ -61,17 +78,21 @@ public class GravityForce implements Screen {
         uicamera.setToOrtho(false, 800, 480);
 
         //Rakete erstellen
-        rocket = new Sprite(rocketImage);
-        rocket.setScale(0.5f);
-        rocket.setX(400 - 32);
-        rocket.setY(240 - 32);
+        rocket = rock.getRocket();
+        rocketEngineSprite = rock.getRocketEngine();
 
+        //Die Rectangles für die Control Buttons
         leftButton = new Rectangle(0, 0, 80, 80);
         rightButton = new Rectangle(100, 0, 80, 80);
         boostButton = new Rectangle(700, 0, 100, 100);
         touchPos = new Vector3();
-    }
 
+        //Soundeffekte
+        thrust_sound = Gdx.audio.newSound(Gdx.files.internal("thrust.mp3"));
+        volume = 0.5f;
+        wasPlayed = false;
+        soundbuffer = 0;
+    }
 
     @Override
     public void show() {
@@ -81,29 +102,15 @@ public class GravityForce implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.GRAY);
-
+        camera.update();
         //Kamera folgt der Rakete
         position = camera.position;
         position.x += (rocket.getX() - position.x) * lerp;
         position.y += (rocket.getY() - position.y) * lerp;
         camera.position.set(position.x, position.y, 0);
 
-        camera.update();
-
-        //Rakete und Background werden projiziert
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(background, -100, -100, 3840, 2160);
-        rocket.draw(batch);
-        batch.end();
-
-        //Das UI wird projiziert
-        UiBatch.setProjectionMatrix(uicamera.combined);
-        UiBatch.begin();
-        UiBatch.draw(leftArrow, leftButton.x, leftButton.y, leftButton.width, leftButton.height);
-        UiBatch.draw(rightArrow, rightButton.x, rightButton.y, rightButton.width, rightButton.height);
-        UiBatch.draw(boostTexture, boostButton.x, boostButton.y, boostButton.width, boostButton.height);
-        UiBatch.end();
+        //Animation der Engine
+        rock.update(Gdx.graphics.getDeltaTime());
 
         // Steuerung der Rakete
         controlRocket();
@@ -111,6 +118,25 @@ public class GravityForce implements Screen {
         // Rakete im Screen behalten
         //keepRocketInScreen();
 
+        playThrustSound();
+
+        //Rakete und Background werden projiziert
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(background, -100, -100, 3840, 2160);
+        rocket.draw(batch);
+        if (moving) rocketEngineSprite.draw(batch);
+        batch.end();
+
+        //Das UI wird projiziert
+        uiBatch.setProjectionMatrix(uicamera.combined);
+        uiBatch.begin();
+        uiBatch.draw(leftArrow, leftButton.x, leftButton.y, leftButton.width, leftButton.height);
+        uiBatch.draw(rightArrow, rightButton.x, rightButton.y, rightButton.width, rightButton.height);
+        uiBatch.draw(boostTexture, boostButton.x, boostButton.y, boostButton.width, boostButton.height);
+        uiBatch.end();
+
+        moving = false;
     }
 
     @Override
@@ -134,7 +160,6 @@ public class GravityForce implements Screen {
     }
 
     public void controlRocket() {
-
         // Schleife über alle Touchpoints (Finger 1 bis 5)
         for (int i = 0; i < 5; i++) {
             if (!Gdx.input.isTouched(i)) continue;
@@ -142,33 +167,38 @@ public class GravityForce implements Screen {
             uicamera.unproject(touchPos.set(Gdx.input.getX(i), Gdx.input.getY(i), 0));
             // Überprüfungen ob man auf die Buttons geklickt hat
             if (leftButton.contains(touchPos.x, touchPos.y)) {
-                rocket.setRotation(rocket.getRotation() + 200 * Gdx.graphics.getDeltaTime());
+                rocket.setRotation(rocket.getRotation() + THRUST * Gdx.graphics.getDeltaTime());
+                rocketEngineSprite.setRotation(rocket.getRotation() + THRUST * Gdx.graphics.getDeltaTime());
             }
             if (rightButton.contains(touchPos.x, touchPos.y)) {
-                rocket.setRotation(rocket.getRotation() - 200 * Gdx.graphics.getDeltaTime());
+                rocket.setRotation(rocket.getRotation() - THRUST * Gdx.graphics.getDeltaTime());
+                rocketEngineSprite.setRotation(rocket.getRotation() - THRUST * Gdx.graphics.getDeltaTime());
             }
             if (boostButton.contains(touchPos.x, touchPos.y)) {
                 moving = true;
             }
         }
-
         velocity(moving);
-        moving = false;
     }
 
     public void velocity(boolean moving) {
         if (moving) {
-            if (currentVelocity < MAX_VELOCITY) {
-                currentVelocity += 10;
+            if (CURRENT_VELOCITY < MAX_VELOCITY) {
+                CURRENT_VELOCITY += 10;
             }
         } else {
-            if (currentVelocity > 0) {
-                currentVelocity -= 5;
+            if (CURRENT_VELOCITY > 0) {
+                CURRENT_VELOCITY -= 5;
             }
         }
-        rocket.translateX((float) (Math.cos(Math.toRadians(rocket.getRotation() + 90)) * currentVelocity * Gdx.graphics.getDeltaTime()));
-        rocket.translateY((float) (Math.sin(Math.toRadians(rocket.getRotation() + 90)) * currentVelocity * Gdx.graphics.getDeltaTime()));
-        rocket.setY(rocket.getY() + gravity * Gdx.graphics.getDeltaTime());
+        // Bewegung der Rakete
+        rocket.translateX((float) (Math.cos(Math.toRadians(rocket.getRotation() + 90)) * CURRENT_VELOCITY * Gdx.graphics.getDeltaTime()));
+        rocket.translateY((float) (Math.sin(Math.toRadians(rocket.getRotation() + 90)) * CURRENT_VELOCITY * Gdx.graphics.getDeltaTime()));
+        rocketEngineSprite.translateX((float) (Math.cos(Math.toRadians(rocket.getRotation() + 90)) * CURRENT_VELOCITY * Gdx.graphics.getDeltaTime()));
+        rocketEngineSprite.translateY((float) (Math.sin(Math.toRadians(rocket.getRotation() + 90)) * CURRENT_VELOCITY * Gdx.graphics.getDeltaTime()));
+        // Gravitation
+        rocket.setY(rocket.getY() + GRAVITY * Gdx.graphics.getDeltaTime());
+
 
     }
 
@@ -191,9 +221,40 @@ public class GravityForce implements Screen {
         }
     }
 
+    public void playThrustSound() {
+        //Buffer weil der Sound noch geladen werden muss
+        soundbuffer += Gdx.graphics.getDeltaTime();
+
+        // Abspielen des Tons bei der ersten Bewegung & nach Bufferzeit
+        if (!wasPlayed && soundbuffer > 2) {
+            sound_id = thrust_sound.play(volume);
+            thrust_sound.setLooping(sound_id, true);
+            wasPlayed = true;
+            System.out.println("Played :" + soundbuffer);
+        }
+
+        //Bei Bewegung wird der Sound lauter
+        if (moving && volume + 0.75f * Gdx.graphics.getDeltaTime() < 1) {
+            thrust_sound.setVolume(sound_id, volume += 0.75f * Gdx.graphics.getDeltaTime());
+        }
+
+        //Bei Stillstand wird der Sound schnell leiser
+        if (volume - 2 * Gdx.graphics.getDeltaTime() > 0 && !moving)
+            thrust_sound.setVolume(sound_id, volume -= 2 * Gdx.graphics.getDeltaTime());
+        System.out.println(volume);
+    }
+
     @Override
     public void dispose() {
         batch.dispose();
+        uiBatch.dispose();
         rocketImage.dispose();
+        rocketEngine.dispose();
+        leftArrow.dispose();
+        rightArrow.dispose();
+        boostTexture.dispose();
+        background.dispose();
+        thrust_sound.dispose();
+
     }
 }
---
+++ core/src/com/mygdx/game/Rocket.java
@@ -0,0 +1,70 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Rocket {
+    Texture rocketImage;
+    Texture rocketEngine;
+
+    public static Sprite rocket;
+    public static Sprite rocketEngineSprite;
+
+    static float ROCKET_SCALE = 0.5f;
+    static float ENGINE_SCALE = 0.6f;
+
+    //Variablen für die Animation der Engine
+    static Animation<TextureRegion> rocketAnimation;
+    private static final int FRAME_COLS = 8, FRAME_ROWS = 1;
+    private static final float FRAME_DURATION = 0.125f;
+    float stateTime;
+
+
+    public Rocket(){
+        //Rocket initialisieren
+        rocketImage = new Texture("Nairan - Battlecruiser - Base.png");
+        rocket = new Sprite(rocketImage);
+        rocket.setScale(ROCKET_SCALE);
+        rocket.setX(400 -32);
+        rocket.setY(240 -32);
+
+        //Rocket Engine initialisieren
+        rocketEngine = new Texture("Nairan - Battlecruiser - Engine.png");
+        TextureRegion[][] tmp = TextureRegion.split(rocketEngine, rocketEngine.getWidth() / FRAME_COLS, rocketEngine.getHeight() / FRAME_ROWS);
+        TextureRegion[] rocketFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                rocketFrames[index++] = tmp[i][j];
+            }
+        }
+        rocketAnimation = new Animation<>(FRAME_DURATION, rocketFrames);
+
+        rocketEngineSprite = new Sprite(rocketFrames[0]);
+        rocketEngineSprite.setScale(ENGINE_SCALE);
+        stateTime = 0f;
+    }
+
+    //Rocket Engine an der Rakete ausrichten
+    public void update(float delta){
+        stateTime += delta;
+        rocketEngineSprite.setRegion(rocketAnimation.getKeyFrame(stateTime, true));
+        rocketEngineSprite.setPosition(rocket.getX(), rocket.getY());
+    }
+
+    public Sprite getRocket(){
+        return rocket;
+    }
+    public Sprite getRocketEngine(){
+        return rocketEngineSprite;
+    }
+
+
+    public void dispose(){
+        rocketImage.dispose();
+        rocketEngine.dispose();
+    }
+
+}
