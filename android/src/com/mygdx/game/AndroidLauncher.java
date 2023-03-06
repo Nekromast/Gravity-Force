fb4484e646c6f644ee2c53e618ffa82d452c97e0
Thomas <thomasko@gmx.net>
2023-03-06 12:03:09 +0100

    - Added a dependency for the constraints in the mainmenu - Added some missing constraings in the mainmenu - Made the mainmenu fullscreen via AndroidLauncher - Exit button now works

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,7 +1,10 @@
 package com.mygdx.game;
 
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -15,18 +18,30 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
+		// Set Mainmenu to fullscreen
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
+		if (Build.VERSION.SDK_INT < 16) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		} else {
+			View decorView = getWindow().getDecorView();
+			int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+			decorView.setSystemUiVisibility(uiOptions);
+		}
 
 		//initialize(new Game(), config);
-
 		initializeForView(new Game(), config);
+
 		//MainMenu per XML View
 		setContentView(R.layout.mainmenu);
 		Button startButton = findViewById(R.id.button_start);
 		startButton.setOnClickListener(v -> {
-			AndroidApplicationConfiguration config1 = new AndroidApplicationConfiguration();
-			config1.useAccelerometer = false;
-			config1.useCompass = false;
-			initialize(new Game(), config1);
+			initialize(new Game(), config);
+		});
+		Button exitButton = findViewById(R.id.button_exit);
+		exitButton.setOnClickListener(v -> {
+			finish();
 		});
 
 	}
