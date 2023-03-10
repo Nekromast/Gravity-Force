ba03bd0dc3e07181fb1a09119d1c164514344561
FMEMike <102667975+FMEMike@users.noreply.github.com>
2023-03-10 23:11:33 +0100

    comment added for GameOverListener

--- core/src/com/mygdx/game/GameOverListener.java
+++ core/src/com/mygdx/game/GameOverListener.java
@@ -1,5 +1,9 @@
 package com.mygdx.game;
 
+
 public interface GameOverListener {
+    /**
+     * @param isGameOver true wenn das Spiel vorbei ist
+     */
     void onGameOverChanged(boolean isGameOver);
 }
